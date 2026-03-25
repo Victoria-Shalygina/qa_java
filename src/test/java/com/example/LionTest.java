490e184c2bc64ffaa1ac52919dc14bff183fcce4
Victoria Shalygina <t4dasko.ru@yandex.ru>
2026-03-25 20:23:23 +0000

    Обновлен gitignore, исключен target

---
+++ src/test/java/com/example/LionTest.java
@@ -0,0 +1,53 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getKittensTest() throws Exception {
+        Mockito.when(feline.getKittens()).thenReturn(5);
+
+        Lion lion = new Lion(feline, "Самец");
+
+        Assert.assertEquals(5, lion.getKittens());
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Mockito.when(feline.getFood("Хищник"))
+                .thenReturn(List.of("Животные", "Птицы", "Рыба"));
+
+        Lion lion = new Lion(feline, "Самец");
+
+        Assert.assertEquals(3, lion.getFood().size());
+    }
+
+    @Test
+    public void lionMaleTest() throws Exception {
+        Lion lion = new Lion(feline, "Самец");
+        Assert.assertTrue(lion.doesHaveMane());
+    }
+
+    @Test
+    public void lionFemaleTest() throws Exception {
+        Lion lion = new Lion(feline, "Самка");
+        Assert.assertFalse(lion.doesHaveMane());
+    }
+
+    @Test(expected = Exception.class)
+    public void lionWrongSexTest() throws Exception {
+        new Lion(feline, "неизвестно");
+    }
+}
